9da366ca295e5524a077aba88f290b20e0630c89
hdmamin <hmamin2@dons.usfca.edu>
2025-03-28 22:42:54 -0700

    lex parens

--- app/main.py
+++ app/main.py
@@ -1,4 +1,82 @@
+from dataclasses import dataclass
+from functools import lru_cache
+import logging
 import sys
+
+
+logger = logging.getLogger()
+logging.basicConfig()
+
+
+@dataclass
+class TokenType:
+    name: str
+    lexeme: str
+    has_literal: bool = False
+
+class TokenTypes:
+
+    @classmethod
+    @lru_cache()
+    def _lexemes2names(cls) -> dict:
+        # Returns a mapping from each lexeme (e.g. "{") to its corresponding TokenType object.
+        return {
+            v.lexeme: v for k, v in vars(cls).items()
+            if k.isupper() and isinstance(v, TokenType)
+        }
+
+    @classmethod
+    def lexeme2name(cls, lexeme: str) -> TokenType:
+        """Maps a single lexeme, e.g. "}", to its coresponding TokenType."""
+        return cls._lexemes2names()[lexeme]
+
+    # Punctuators
+    # (){};,+-*!===<=>=!=<>/.
+    LEFT_PAREN = TokenType(name="LEFT_PAREN", lexeme="(")
+    RIGHT_PAREN = TokenType(name="RIGHT_PAREN", lexeme=")")
+    LEFT_BRACE = TokenType(name="LEFT_BRACE", lexeme="{")
+    RIGHT_BRACE = TokenType(name="RIGHT_BRACE", lexeme="}")
+    SEMICOLON = TokenType(name="SEMICOLON", lexeme=";")
+    COMMA = TokenType(name="COMMA", lexeme=",")
+    PLUS = TokenType(name="PLUS", lexeme="+")
+    MINUS = TokenType(name="MINUS", lexeme="-")
+    STAR = TokenType(name="STAR", lexeme="*")
+    BANG_EQUAL = TokenType(name="BANG_EQUAL", lexeme="!=")
+    EQUAL_EQUAL = TokenType(name="EQUAL_EQUAL", lexeme="==")
+    LESS_EQUAL = TokenType(name="LESS_EQUAL", lexeme="<=")
+    GREATER_EQUAL = TokenType(name="GREATER_EQUAL", lexeme=">=")
+    BANG_EQUAL = TokenType(name="BANG_EQUAL", lexeme="!=")
+    LESS = TokenType(name="LESS", lexeme="<")
+    GREATER = TokenType(name="GREATER", lexeme=">")
+    SLASH = TokenType(name="SLASH", lexeme="/")
+    DOT = TokenType(name="DOT", lexeme=".")
+    EOF = TokenType(name="EOF", lexeme=" ")
+
+
+class Token:
+    def __init__(self, value: str):
+        self.token_type = TokenTypes.lexeme2name(value)
+        self.value = value
+
+    def lexed(self) -> str:
+        res = f"{self.token_type.name} {self.token_type.lexeme} "
+        if self.token_type.has_literal:
+            res += self.value
+        else:
+            res += "null"
+        return res
+
+
+def tokenize(source: str) -> list[str]:
+    """Each str contains one line of lexed source code, e.g.
+    'STRING "dog" dog'
+    """
+    res = []
+    for char in source:
+        token = Token(char)
+        res.append(token.lexed())
+    res.append("EOF  null")
+    return res
 
 
 def main():
@@ -20,10 +98,8 @@ def main():
     print("Logs from your program will appear here!", file=sys.stderr)
 
     # Uncomment this block to pass the first stage
-    if file_contents:
-        raise NotImplementedError("Scanner not implemented")
-    else:
-        print("EOF  null") # Placeholder, remove this line when implementing the scanner
+    lexed = tokenize(file_contents)
+    print("\n".join(lexed))
 
 
 if __name__ == "__main__":
