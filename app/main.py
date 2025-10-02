f60647d84ee738fb35c1365294f3a22767778ab6
hdmamin <hmamin2@dons.usfca.edu>
2025-10-02 11:43:52 -0700

    flesh out streamlit app

--- app/main.py
+++ app/main.py
@@ -6,7 +6,41 @@ import tempfile
 from lox.main import main
 
 
-source = st.text_area("Lox Program", height=300)
+st.markdown(
+    """
+    # Lox Interpreter
+    An implementation of the Lox programming language defined in
+    [Crafting Interpreters](https://craftinginterpreters.com/).
+    """
+)
+with st.expander("Syntax Cheatsheet"):
+    st.markdown(
+        """
+        ```lox
+        // Comments
+        print "Hello"; 
+        var x = 42; x = x + 1;
+
+        // Types: number, string "s", true/false, nil
+
+        // Control
+        if (x > 10) print "big"; else print "small";
+        while (x < 5) { print x; x = x + 1; }
+        for (var i = 0; i < 3; i = i + 1) print i;
+
+        // Functions
+        fun greet(name) { print "Hi " + name; }
+        greet("Ada");
+
+        // Classes
+        class A { speak() { print "noise"; } }
+        class B < A { speak() { print "woof"; } }
+        var d = B(); d.speak();
+
+        // Operators: == != ! and or < <= > >=
+        """
+    )
+source = st.text_area("Write a lox program below", placeholder='print "hello word";', height=300)
 if source:
     try:
         # hack: turns out lexing is slightly harder if we don't have readlines() to rely on in
